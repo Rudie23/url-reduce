b09efa5b588b5deeb66f8a89c51ed8aaeae1659e
Diego <ruandiego100@gmail.com>
2022-08-07 16:43:02 -0300

    Parte 3

--- devpro/encurtador/urls.py
+++ devpro/encurtador/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from devpro.encurtador import views
 
 urlpatterns = [
-    path('<slug:slug>', views.redirect1),
+    path('', views.home, name='home'),
+    path('<slug:slug>', views.redirecturl, name='redirecturl'),
+    path('relatorios/', views.relatorio, name='relatorio'),  # Tive que criar uma função nova
+    path('relatorios/<slug:slug>', views.relatorios, name='relatorios'),
+
 ]
