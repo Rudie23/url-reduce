19180e3e620dfe48ae495a21d361225c1a326c18
Diego <ruandiego100@gmail.com>
2022-08-08 14:52:06 -0300

    Parte 4: criado a primeira parte do registro dos logs.

--- devpro/encurtador/views.py
+++ devpro/encurtador/views.py
@@ -1,7 +1,9 @@
+from django.db.models import Count
+from django.db.models.functions import TruncDate
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from devpro.encurtador.models import UrlRedirect
+from devpro.encurtador.models import UrlRedirect, UrlLog
 
 
 def home(request):
@@ -17,12 +19,23 @@ def relatorios(request, slug):
     # parâmetro que está sendo passado na função
     # Para criar uma url completa, e incluindo o esquema e o domínio. Ela recebe como parametro
     url_reduzida = request.build_absolute_uri(f'/{slug}')
+
     ctx = {'reduce': url_redirect,
-           'url_reduzida': url_reduzida}
+           'url_reduzida': url_reduzida,
+
+           }
     return render(request, 'base/relatorio.html', ctx)
 
 
+# https://docs.djangoproject.com/en/4.1/ref/request-response/
 def redirecturl(request, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)  # Eu quero que o slug do meu UrlRedirect seja igual ao do
     # parâmetro que está sendo passado na função
+    UrlLog.objects.create(
+        origem=request.META.get('HTTP_REFERER'),
+        user_agent=request.META.get('HTTP_USER_AGENT'),
+        host=request.META.get('HTTP_HOST'),
+        ip=request.META.get('REMOTE_ADDR'),
+        url_redirect=url_redirect
+    )
     return redirect(url_redirect.destino)
